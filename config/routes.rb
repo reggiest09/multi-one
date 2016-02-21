5c397846870eaa3fded06a7d0731514055ea835c
Reggie Tucker <reggiest09@gmail.com>
2016-02-21 01:59:01 -0500

    weather_grid

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources :weather
+  resources :weather_grid
   # You can have the root of your site routed with "root"
    root to: "home#index"
 
