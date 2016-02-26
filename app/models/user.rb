fb00ceb253e1bccbf362949419c26f0f666fce6b
Reggie Tucker <reggiest09@gmail.com>
2016-02-26 02:15:13 -0500

    deleted

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,15 +2,15 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable, 
-         :recoverable, :rememberable, :trackable, :validatable, :confirmable,
-         :async
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
+         
 
 validate :email_is_unique, on: :create
 after_create :create_account
 
-#def confirmation_required?
- #false
-#end
+def confirmation_required?
+ false
+end
 
 
 
